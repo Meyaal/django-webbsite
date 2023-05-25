17fd41019268dc3a6b5914ceceb415b8ad45513f
Meyaal <alsuhaili_32@hotmail.com>
2023-05-25 15:54:35 +0000

    create function in viwes

--- Allakando/restaurant/urls.py
+++ Allakando/restaurant/urls.py
@@ -2,5 +2,10 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.webbsite, name='webbsite'),
+    path('', views.home, name='home'),
+    path('booking/', views.booking_page, name='booking_page'),
+    path('mybookings/', views.mybookings_page, name='mybookings_page'),
+    path('changebooking/', views.edit_booking, name='edit'),
+    path('deletebooking/', views.delete_booking, name='delete'),
+
 ]
--- Allakando/restaurant/views.py
+++ Allakando/restaurant/views.py
@@ -1,5 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
 
 # Create your views here.
-def webbsite(request):
+def home(request):
     return render(request, 'index.html')
+
+def booking_page(request):
+    if request.method == 'POST':
+        return redirect("mybookings_page")
+    return render(request, 'booking.html')
+
+def mybookings_page(request):
+    return render(request, 'mybookings.html')
+
+def edit_booking(request):
+    return render(request, 'edit_booking.html')
+
+def delete_booking(request):
+    return redirect('mybookings_page')
+
+
